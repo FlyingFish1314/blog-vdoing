fdfccd961152c7186cc272f6a3a2a7e994ed5090
范恩龙 <1732819972@qq.com>
2023-04-20 20:35:27 +0800

    init

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,26 +1,47 @@
-// import vue from 'vue/dist/vue.esm.browser'
-export default ({
-  Vue, // VuePress 正在使用的 Vue 构造函数
-  options, // 附加到根实例的一些选项
-  router, // 当前应用的路由实例
-  siteData // 站点元数据
-}) => {
-  // window.Vue = vue // 使页面中可以使用Vue构造函数 （使页面中的vue demo生效）
+const head = require('./config/head.js');
+const plugins = require('./config/plugins.js');
+const themeConfig = require('./config/themeConfig.js');
+
+module.exports = {
+  theme: 'vdoing', // 使用npm包主题
+  title: "blog-vdoing",
+  description: 'vdoing博客主题模板',
+  base: '/blog-vdoing/', // 格式：'/<仓库名>/'， 默认'/'
+  markdown: {
+    lineNumbers: true, // 代码行号
+  },
+
+  head,
+  plugins,
+  themeConfig,
+
+  plugins: [
+    // vssue 评论插件
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "3496ea08107b9e30d7b6",
+        clientSecret: "9bd845664fcc60d17892fa345f2e8ac89cc0d1d0",
+        owner: "lql95",
+        repo: "blog-vdoing",
+      },
+    ],
+    // Markdown 增强
+    [
+      "md-enhance",
+      {
+        // 启用流程图
+        flowchart: true,
+        // Enable mermaid
+        mermaid: true,
+        // 启用 TeX 支持
+        tex: true,
+      },
+    ],
+  ],
 }
-
-// export default ({ router }) => {
-//   router.beforeEach((to, from, next) => {
-//     console.log("切换路由", to.fullPath, from.fullPath);
-
-//     //触发百度的pv统计
-//     if (typeof _hmt != "undefined") {
-//       if (to.path) {
-//         _hmt.push(["_trackPageview", to.fullPath]);
-//         console.log("上报百度统计", to.fullPath);
-//       }
-//     }
-
-//     // continue
-//     next();
-//   });
-// };
